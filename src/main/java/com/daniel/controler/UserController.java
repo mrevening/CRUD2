243c8a40c698a3f70a07fa7f2dbcc47f82d1c094
Dominik Wieczorek <dominicwieczo@gmail.com>
2015-06-21 23:48:37 +0200

    start

---
+++ src/main/java/com/daniel/controler/UserController.java
@@ -0,0 +1,7 @@
+package com.daniel.controler;
+
+/**
+ * Created by mrevening on 2015-06-21.
+ */
+public class UserController {
+}
---
+++ src/main/java/com/daniel/model/User.java
@@ -0,0 +1,7 @@
+package com.daniel.model;
+
+/**
+ * Created by mrevening on 2015-06-21.
+ */
+public class User {
+}
